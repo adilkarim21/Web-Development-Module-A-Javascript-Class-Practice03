e76770a1f6732d766dc4bc108c411957da5f8c52
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-09-22 22:31:01 +0500

    added files

---
+++ app.js
@@ -0,0 +1,169 @@
+// var name = prompt("Enter Your Name")
+// var age = +prompt("Enter Your Age")
+// var city = prompt("Enter Your City")
+
+// var arr = [
+//   prompt("Enter Your Name"),
+//   +prompt("Enter Your Age"),
+//   prompt("Enter Your City"),
+// ];
+
+// var userArr = [prompt("Enter your first name"),
+//     prompt("Enter your age"),
+
+// ]
+
+
+
+
+
+// --------------------------------------------------------------------------
+
+
+
+// 1.)Push Method:
+
+// var fruitsArr = ["apple","banana","mango","pine apple","orange"];
+
+// fruitsArr.push("faraz",120)
+
+
+// console.log(fruitsArr);
+
+
+
+
+// 2.)Unshift Method:
+
+// var fruitsArr = ["apple","banana","mango","pine apple","orange"];
+
+// fruitsArr.unshift("faraz",120)
+
+
+// console.log(fruitsArr);
+
+
+
+
+// 3.)
+
+
+
+
+
+
+
+// 5.)Splice Method:
+
+
+// a.) Deleting:
+
+// var fruitsArr = ["apple","banana","mango","pine apple","orange"];
+
+// fruitsArr.splice(2,3)
+
+
+// console.log(fruitsArr);
+
+
+// b.) Adding:
+
+// var fruitsArr = ["apple","banana","mango","pine apple","orange"];
+
+// fruitsArr.splice(4,0,"faraz",500,true)
+
+
+// console.log(fruitsArr);
+
+
+// c.) Both Add & Delete:
+
+// var fruitsArr = ["apple","banana","mango","pine apple","orange"];
+
+// fruitsArr.splice(-1,0,"faraz")
+
+
+// console.log(fruitsArr);
+
+
+
+
+// 6.) Slice:
+
+// var fruitsArr = ["apple","banana","mango","pine apple","orange"];
+
+// var copy = fruitsArr.slice(0,3)
+
+
+// console.log(copy);
+
+
+
+
+// -------------------------------------------------------------------------
+
+
+
+// var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+// country_list.push("faraz",120)
+
+// country_list.unshift("adil",100)
+
+// country_list.pop()
+
+// country_list.shift()
+
+// country_list.splice(-1,0,"faraz")
+
+// country_list.slice(1,12)
+
+
+// console.log(country_list);
+
+
+
+
+// -------------------------------------------------------------------
+
+
+
+
+// Loops:
+
+
+// console.log("JavaScript Class");
+// console.log("JavaScript Class");
+// console.log("JavaScript Class");
+// console.log("JavaScript Class");
+// console.log("JavaScript Class");
+// console.log("JavaScript Class");
+// console.log("JavaScript Class");
+// console.log("JavaScript Class");
+// console.log("JavaScript Class");
+// console.log("JavaScript Class");
+
+
+// bad practice / good practice
+
+
+// 1.)For Loop:
+
+
+// for(var i = 0;i<10;i++) {
+//     document.write("JavaScript Class <br>");
+// }
+
+// for(var i = 0;i<10;i++) {
+//     console.log(i,"Hello World");
+// }
+
+// for ( var i = 0 ; i < 15 ; ++i ) {
+//     document.write( "Air Jordan <br> " )
+// }
+
+
+// 2.)While Loop:
+
+
+// 3.)Do-while Loop:
